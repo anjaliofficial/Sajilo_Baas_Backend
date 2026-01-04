0357aff81d6388c96ea573424b19dcaacccf5152
anjaliofficial <bistaanjali1415@gmail.com>
2026-01-04 11:26:15 +0545

    Index.ts in config

---
+++ src/database/mongodb.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import { MONGODB_URI } from '../config';
+
+export async function connectToDatabase(){
+    try{
+        await mongoose.connect(MONGODB_URI);
+        console.log('Connected to MongoDB successfully.');
+
+    } catch (error){
+        console.error('Error Connecting To MongoDB', error);
+        process.exit(1);
+    }
+}
