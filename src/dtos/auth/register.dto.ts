eabe824e0cdd82fcfbdee775a2318d5b8843573f
anjaliofficial <bistaanjali1415@gmail.com>
2026-01-04 17:54:24 +0545

    register.dto.ts

---
+++ src/dtos/auth/register.dto.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const registerDTO = z.object({
+    email: z.string().email(),
+    password: z.string().min(6,'Password must be at least 6 characters'), 
+    confirmPassword: z.string().min(6),
+    address: z.string().min(3, 'Address is required'),
+    contactNumber: z.string().regex(/^[0-9]{7,15}$/, 'Invalid contact number'),
+
+}).refine((data)=> data.password === data.confirmPassword, {
+    message: 'Password do not match',
+    path: ['confirmPassword'],
+});
